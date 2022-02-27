ccb61e4f5d8d052ad98087a5f4bc518c5c2c0d02
esther615kim <vilchen@naver.com>
2022-02-28 08:18:19 +0900

    feat: add showDetails function

--- src/components/Cards/Work/ProjectCard.tsx
+++ src/components/Cards/Work/ProjectCard.tsx
@@ -1,40 +1,70 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { IProject } from "../../../../types";
-import {Link} from "react-router-dom";
-import { AiFillGithub, AiFillProject } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import {
+  AiFillGithub,
+  AiOutlineFundProjectionScreen,
+  AiOutlineCloseCircle,
+} from "react-icons/ai";
 
 const ProjectCard: FunctionComponent<{
   project: IProject;
 }> = ({
-  project: { name, image_url, category, description, github_url,technologies, deployed_url },
+  project: {
+    name,
+    image_url,
+    category,
+    description,
+    github_url,
+    technologies,
+    deployed_url,
+  },
 }) => {
+  const [displayDetails, setDisplayDetails] = useState<boolean>(false);
+
+  const handleClickShowDetails = ()=>{
+      console.log("show more!")
+      setDisplayDetails(prev => !prev);
+  }
   return (
     <div className="text-stone-200 bg-zinc-800">
-        <div>
-        <img className="object-cover h-50 w-100 cursor-pointer rounded-sm" src={image_url} alt={name} />
+      <div>
+        <img
+          className="object-cover h-50 w-100 cursor-pointer rounded-sm"
+          src={image_url}
+          alt={name}
+          onClick={handleClickShowDetails}
+        />
         <p className="p-2"> {name}</p>
-        </div>
+      </div>
 
-        {/* details */}
+      {/* details */}
+      {displayDetails && (
         <div className="grid md:grid-cols-2">
-            <div>
-            <img src={image_url} alt={name}/>
+          <div>
+            <img src={image_url} alt={name} />
             <a href={github_url}>
-                <AiFillGithub/> <span>Github</span>
+              <AiFillGithub /> <span>Github</span>
             </a>
             <a href={github_url}>
-                <AiFillProject/> <span>Project</span>
+              <AiOutlineFundProjectionScreen /> <span>Project</span>
             </a>
-            </div>
+          </div>
+          <div>
+            <h3>{name}</h3>
+            <h4>{description}</h4>
             <div>
-                <h3>{name}</h3>
-                <h4>{description}</h4>
-                <div>
-                    {technologies.map(item=>(<span key={item}>{item}</span>))}
-                </div>
+              {technologies.map((item) => (
+                <span key={item}>{item}</span>
+              ))}
             </div>
-        
+          </div>
+          {/* remove button */}
+          <button onClick={handleClickShowDetails}>
+            <AiOutlineCloseCircle />
+          </button>
         </div>
+      )}
     </div>
   );
 };
