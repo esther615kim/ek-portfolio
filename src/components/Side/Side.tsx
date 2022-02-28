69ee3ed10349ff30ab41196db0a39e8d313047f2
esther615kim <vilchen@naver.com>
2022-03-01 05:54:33 +0900

    add links in Side components

--- src/components/Side/Side.tsx
+++ src/components/Side/Side.tsx
@@ -10,25 +10,23 @@ import { GoLocation } from "react-icons/go";
 const Side = () => {
   return (
     <div>
-      <h3 className="lg:mt-20 lg:pt-40 mt-10 text-3xl font-semibold text-white tracking-wider font-oohhbaby">
+      <h3 className="mt-10 text-3xl font-semibold tracking-wider text-white lg:mt-20 lg:pt-40 font-oohhbaby">
         Esther Kim
       </h3>
-      <p className="hover:text-cyan-300 font-poppins mt-5 mb-20 lg:mb-40 font-medium text-lg ">
+      <p className="mt-5 mb-20 text-lg font-medium hover:text-cyan-300 font-poppins lg:mb-40 ">
         Front End Developer
       </p>
-      {/* <p className="px-2 py-1 mx-4 mt-4 font-semibold bg-stone-100 rounded-md"> */}
-      {/* Front End Developer */}
-      {/* </p> */}
 
       {/* socials */}
-      <div className="bg-gradient-to-r from-zinc-700 to-zinc-800 flex justify-around py-4 md:px-10 px-20 space-x-2 my-5 text-stone-100 md:w-auto md:2-full">
-        {/* <Link to="https://github.com/esther615kim"> */}
-        <AiOutlineLinkedin className="hover:text-cyan-300 hover:scale-105 w-6 h-6 cursor-pointer" />
-        {/* </Link> */}
-        <AiFillGithub className="hover:text-cyan-300 w-6 h-6 cursor-pointer" />
-        <AiFillCodeSandboxCircle className="hover:text-cyan-300 w-6 h-6 cursor-pointer" />
+      <div className="flex justify-around px-20 py-4 my-5 space-x-2 bg-gradient-to-r from-zinc-700 to-zinc-800 md:px-10 text-stone-100 md:w-auto md:2-full">
+        <AiOutlineLinkedin className="w-6 h-6 cursor-pointer hover:text-cyan-300 hover:scale-105" />
+        <AiFillGithub 
+         onClick={()=>{window.location.href="https://github.com/esther615kim"}} 
+        className="w-6 h-6 cursor-pointer hover:text-cyan-300" />
+        <AiFillCodeSandboxCircle 
+                 onClick={()=>{window.location.href="https://codepen.io/EKim15"}} 
+        className="w-6 h-6 cursor-pointer hover:text-cyan-300" />
       </div>
-      {/* <div className="py-3 my-5 bg-stone-200"> */}
       <div className="py-3 my-5">
         <div className="flex items-center justify-center space-x-1 space-y-3">
           <GoLocation className="w-5 h-5" />
@@ -36,19 +34,20 @@ const Side = () => {
         </div>
         <p className="my-2 font-rubik">vilchen15@gmail.com</p>
       </div>
-      {/* <p className="px-2 py-1 mx-4 mt-4 font-semibold rounded-md bg-gradient-to-r from-stone-400 to-stone-300">
-        CV
-      </p> */}
-      <div className="space-y-2 pb-10 lg:pb-20">
-        <button className="hover:bg-cyan-300 hover:scale-95 shadow-md font-poppins w-10/12 py-1 my-1 rounded-md mb-2 bg-gradient-to-r from-stone-100 to-gray-200">
+      <div className="pb-10 space-y-2 lg:pb-20">
+        <button
+        onClick={()=>{window.location.href="https://resume.io/r/6KtYGMKR3"}} 
+        className="w-10/12 py-1 my-1 mb-2 rounded-md shadow-md hover:bg-cyan-300 hover:scale-95 font-poppins bg-gradient-to-r from-stone-100 to-gray-200">
           CV
         </button>
-        <button className="hover:scale-95 shadow-md font-poppins w-10/12 py-1 my-1 text-white rounded-md bg-gradient-to-r from-red-300 to-red-400">
+        <button className="w-10/12 py-1 my-1 text-white rounded-md shadow-md hover:scale-95 font-poppins bg-gradient-to-r from-red-300 to-red-400">
           Contact Me
         </button>
       </div>
     </div>
   );
 };
+
+
 
 export default Side;
