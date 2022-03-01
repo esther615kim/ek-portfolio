2bb577037113fc1551af4367b408dcf12475d4f0
esther615kim <vilchen@naver.com>
2022-03-01 09:27:06 +0900

    Misc.

--- src/components/Cards/Contact/index.tsx
+++ src/components/Cards/Contact/index.tsx
@@ -1,24 +1,45 @@
-
-import {AiOutlineMail} from "react-icons/ai";
+import { AiOutlineMail, AiOutlineMobile } from "react-icons/ai";
 export default function Contact() {
   return (
-    <div className="h-full p-5 md:p-20 ">
-      <div className="grid h-full pt-10 rounded-lg md:p-20 sm:pt-0 bg-zinc-800 justify-items-end">
-        {/* FORM */}
-      <div className="p-5 rounded-lg md:w-3/5 lg:w-3/5 xl:w-2/5 md:pt-10 bg-stone-200">
-        <h1 className="my-10 text-lg font-bold">Let's talk</h1>
-        <form action="submit">
-          <input className="w-full px-5 py-1 mb-5 rounded-md"type="text" placeholder="FIRST NAME" />
-          <input className="w-full px-5 py-1 mb-5 rounded-md" type="text" placeholder="EMAIL" />
-          <textarea className="w-full px-5 py-1 mb-5 rounded-md h-1/5" placeholder="MESSAGE"></textarea>
-          <button className="w-full px-5 py-1 mb-5 rounded-md shadow-md text-stone-100 hover:scale-95 font-poppins bg-zinc-700"> 
-           SEND
-          </button>
-        </form>
-      </div>
+    <div className="h-full p-5 md:p-20 xl:mx-20 ">
+      <div className="grid h-full grid-cols-2 pt-10 rounded-lg md:p-20 sm:pt-0 bg-zinc-800">
+        <div className="grid content-end py-20 sm:p-10 text-stone-200">
+          <h1 className="ml-1 text-xl font-bold text-left">Contact me.</h1>
 
+          <div className="flex items-center font-semibold text-md ">
+            <AiOutlineMobile className="mr-2" />
+            07349932320
+          </div>
+          <div className="flex items-center font-semibold text-md">
+            <AiOutlineMail className="mr-2" />
+            vilchen15@gmail.com
+          </div>
+
+        </div>
+        {/* FORM */}
+        <div className="p-5 m-5 rounded-lg md:pt-10 bg-stone-200">
+          <h1 className="my-10 text-xl font-bold">Let's talk</h1>
+          <form action="submit">
+            <input
+              className="w-full px-5 py-1 mb-5 rounded-md"
+              type="text"
+              placeholder="FIRST NAME"
+            />
+            <input
+              className="w-full px-5 py-1 mb-5 rounded-md"
+              type="text"
+              placeholder="EMAIL"
+            />
+            <textarea
+              className="w-full px-5 py-1 mb-5 rounded-md h-1/5"
+              placeholder="MESSAGE"
+            ></textarea>
+            <button className="w-full px-5 py-1 mb-5 rounded-md shadow-md text-stone-100 hover:scale-95 font-poppins bg-zinc-700">
+              SEND
+            </button>
+          </form>
+        </div>
       </div>
-    
     </div>
   );
 }
