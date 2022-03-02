0b44920767b6c4c6f2a9cbe535a9b42520540257
esther615kim <vilchen@naver.com>
2022-03-02 11:53:28 +0900

    style:contact page reponsive design layout

--- src/components/Cards/Contact/index.tsx
+++ src/components/Cards/Contact/index.tsx
@@ -1,11 +1,11 @@
 import { AiOutlineMail, AiOutlineMobile } from "react-icons/ai";
 export default function Contact() {
   return (
-    <div className="h-full pt-5 md:mt-20 md:p-10 xl:mx-20 ">
-      <div className="grid h-full grid-cols-2 pt-10 rounded-lg md:p-20 sm:pt-0 bg-zinc-800">
-        <div className="grid content-end py-20 sm:p-10 text-stone-200">
+    <div className="h-full md:mt-20 md:p-10  xl:px-20 ">
+      <div className="grid h-full md:grid-cols-2 rounded-lg bg-zinc-800">
+        
+         <div className="p-5 2xl:px-10 grid content-end md:py-20 text-stone-200">
           <h1 className="ml-1 text-xl font-bold text-left">Contact me.</h1>
-
           <div className="flex items-center font-semibold text-md ">
             <AiOutlineMobile className="mr-2" />
             07349932320
@@ -14,10 +14,10 @@ export default function Contact() {
             <AiOutlineMail className="mr-2" />
             vilchen15@gmail.com
           </div>
+        </div> 
 
-        </div>
         {/* FORM */}
-        <div className="px-5 py-10 m-5 rounded-lg md:pt-10 bg-stone-200">
+        <div className="px-5 py-10 m-5 2xl:m-10 rounded-lg md:pt-10 bg-stone-200">
           <h1 className="my-10 text-xl font-bold">Let's talk</h1>
           <form action="submit">
             <input
@@ -30,7 +30,7 @@ export default function Contact() {
               type="text"
               placeholder="EMAIL"
             />
-            <textarea
+            <textarea style={{height:160}}
               className="w-full px-5 py-1 mb-5 rounded-md"
               placeholder="MESSAGE"
             ></textarea>
