279268a1a22490d33f378e4591a136be229008ab
esther615kim <vilchen@naver.com>
2022-03-11 18:51:20 +0900

    fix: font error

--- src/components/Cards/About/index.tsx
+++ src/components/Cards/About/index.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className="col-span-12 p-10 md:px-10 lg:px-20">
       <div className="lg:pt-10">
         <h3 className="text-xl text-left text-red-400 font-semiboldp">About</h3>
-        <h5 className="block mt-5 text-left dark:text-white md:text-xl font-sacramento">
+        <h5 className="block mt-5 text-left dark:text-white md:text-lg font-rubik">
         I am a JavaScript Developer interested in the React environment and Front-end development technologies.
         </h5>
         <Tech/>
